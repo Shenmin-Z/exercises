21a2f8dd57faccde6810577d24f5b3e33d3c25e5
Shenmin Zhou <sam.s.zhou@newegg.com>
2019-11-22 17:29:35 +0800

    update

--- find/src/index.ts
+++ find/src/index.ts
@@ -1,5 +1,11 @@
-import { Info, FilePath, getUsefulContents, getInfo } from "./file";
-import {join} from 'path'
+import {
+  Info,
+  FilePath,
+  getUsefulContents,
+  getInfo,
+  isDirectory,
+} from "./file";
+import { join } from "path";
 
 class SeedWrapper<S> {
   constructor(public seed: S) {}
@@ -12,29 +18,47 @@ class Skip<S> extends SeedWrapper<S> {}
 class Continue<S> extends SeedWrapper<S> {}
 
 type Iterate<S> = Done<S> | Skip<S> | Continue<S>;
-type Iterator<S> = (a: S) => (b: Info) => Iterate<S>;
+type Iterator<S> = (a: S, b: Info) => Iterate<S>;
 
-async function foldTree<S>(iter: Iterator<S>, initSeed:S, path: FilePath) {
-  const endSeed = await fold(initSeed, path)
-  return Promise.resolve(endSeed.unwrap())
+export async function foldTree<S>(
+  iter: Iterator<S>,
+  initSeed: S,
+  path: FilePath,
+): Promise<S> {
+  const endSeed = await fold(initSeed, path);
+  return endSeed.unwrap();
 
   async function fold(seed: S, subpath: FilePath) {
-    const names = await getUsefulContents(path)
-    return walk(seed, names)
+    const names = await getUsefulContents(subpath);
+    return walk(seed, names);
   }
 
-  function walk(seed: S, [name, ...names]: FilePath[]) {
-    const path_ = join(path, name)
-    const info = getInfo(path_)
-    const seedWrapper = iter(seed)(info)
+  async function walk(
+    seed: S,
+    [name, ...names]: FilePath[],
+  ): Promise<SeedWrapper<S>> {
+    const path_ = join(path, name);
+    const info = getInfo(path_);
+    const seedWrapper = iter(seed, info);
     if (seedWrapper instanceof Done) {
-      return seedWrapper
+      return seedWrapper;
     }
     if (seedWrapper instanceof Skip) {
-      const seed_ = seedWrapper.unwrap()
-      return walk(seed_, names)
+      const seed_ = seedWrapper.unwrap();
+      return walk(seed_, names);
     }
     if (seedWrapper instanceof Continue) {
+      const seed_ = seedWrapper.unwrap();
+      if (isDirectory(info)) {
+        const next = await fold(seed_, path_);
+        if (next instanceof Done) {
+          return next;
+        } else {
+          return walk(next.unwrap(), names);
+        }
+      } else {
+        return walk(seed_, names);
+      }
     }
   }
 }
