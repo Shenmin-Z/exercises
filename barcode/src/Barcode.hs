68292b60820a672acf76e45224f0a4943ab9d712
sz9h <sam.s.zhou@newegg.com>
2019-12-10 09:08:42 +0800

    update

--- barcode/src/Barcode.hs
+++ barcode/src/Barcode.hs
@@ -236,3 +236,69 @@ bestLeft ps = sortBy
 
 bestRight :: [Run] -> [Parity (Score, Digit)]
 bestRight = map None . bestScores rightSRL
+
+-- Candidates
+
+chunkWith :: ([a] -> ([a], [a])) -> [a] -> [[a]]
+chunkWith _ [] = []
+chunkWith f xs = let (h, t) = f xs in h : chunkWith f t
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n = chunkWith (splitAt n)
+
+candidateDigits :: RunLength Bit -> [[Parity Digit]]
+candidateDigits ((_, One) : _)        = []
+candidateDigits rle | length rle < 59 = []
+candidateDigits rle | any null match = []
+                    | otherwise      = map (map (fmap snd)) match
+ where
+  match      = map bestLeft left ++ map bestRight right
+  left       = chunksOf 4 . take 24 . drop 3 $runLengths
+  right      = chunksOf 4 . take 24 . drop 32 $runLengths
+  runLengths = map fst rle
+
+-- input = zip (runLengths $ encodeEAN13 "9780132114677") (cycle [Zero, One])
+
+-- Soup
+
+type Map a = M.Map Digit [a]
+
+type DigitMap = Map Digit
+type ParityMap = Map (Parity Digit)
+
+updateMap :: Parity Digit -> Digit -> [Parity Digit] -> ParityMap -> ParityMap
+updateMap digit key seq = insertMap key (fromParity digit) (digit : seq)
+
+insertMap :: Digit -> Digit -> [a] -> Map a -> Map a
+insertMap key digit val m = val `seq` M.insert key' val m
+  where key' = (key + digit) `mod` 10
+
+useDigit :: ParityMap -> ParityMap -> Parity Digit -> ParityMap
+useDigit old new digit =
+  new `M.union` M.foldrWithKey (updateMap digit) M.empty old
+
+incorporateDigits :: ParityMap -> [Parity Digit] -> ParityMap
+incorporateDigits old = foldl' (useDigit old) M.empty
+
+finalDigits :: [[Parity Digit]] -> ParityMap
+finalDigits =
+  foldl' incorporateDigits (M.singleton 0 []) . mapEveryOther (map (fmap (* 3)))
+
+-- First digit
+
+firstDigit :: [Parity a] -> Digit
+firstDigit =
+  snd . head . bestScores paritySRL . runLengths . map parityBit . take 6
+ where
+  parityBit (Even _) = Zero
+  parityBit (Odd  _) = One
+
+addFirstDigit :: ParityMap -> DigitMap
+addFirstDigit = M.foldrWithKey updateFirst M.empty
+
+updateFirst :: Digit -> [Parity Digit] -> DigitMap -> DigitMap
+updateFirst key seq = insertMap key digit (digit : renormalize qes)
+ where
+  renormalize = mapEveryOther (`div` 3) . map fromParity
+  digit       = firstDigit qes
+  qes         = reverse seq
