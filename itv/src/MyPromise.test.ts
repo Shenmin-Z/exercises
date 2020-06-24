e258e6a219290687e422e0f871b9277b5a0eba53
sz9h <sam.s.zhou@newegg.com>
2020-06-24 18:06:56 +0800

    add itv

---
+++ itv/src/MyPromise.test.ts
@@ -0,0 +1,30 @@
+import { MyPromise } from "./MyPromise";
+
+console.log("start");
+
+new MyPromise(res => {
+  console.log("this should be between start and end");
+  res(999);
+}).then(x => {
+  console.log(x);
+});
+
+new MyPromise(res => {
+  setTimeout(() => {
+    res("print last");
+  }, 0);
+}).then(x => {
+  console.log(x);
+});
+
+MyPromise.resolve(1)
+  .then(x => {
+    console.log(x);
+    return 2;
+  })
+  .then(x => {
+    console.log(x);
+    return { aaa: 123 };
+  });
+
+console.log("end");
---
+++ itv/src/MyPromise.ts
@@ -0,0 +1,45 @@
+export class MyPromise<T, U = any> {
+  onResolve: ((a: T) => any) | null;
+  onReject: ((a: U) => any) | null;
+
+  constructor(fn: (res: (a: T) => void, rej: (e: U) => void) => void) {
+    this.onResolve = null;
+    this.onReject = null;
+    fn(
+      resolved => {
+        process.nextTick(() => {
+          if (this.onResolve !== null) {
+            this.onResolve(resolved);
+          }
+        });
+      },
+      rejected => {
+        if (this.onReject !== null) {
+          this.onReject(rejected);
+        }
+      }
+    );
+  }
+
+  then<V, W = any>(
+    onResolve: (a: T) => V,
+    onReject?: (a: U) => W
+  ): MyPromise<V, W> {
+    return new MyPromise((res, rej) => {
+      this.onResolve = a => {
+        res(onResolve(a));
+      };
+      this.onReject = a => {
+        if (onReject) {
+          rej(onReject(a));
+        }
+      };
+    });
+  }
+
+  static resolve<X>(x: X) {
+    return new MyPromise(res => {
+      res(x);
+    });
+  }
+}
