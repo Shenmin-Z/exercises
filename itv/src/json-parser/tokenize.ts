04a8f8632c7ce6149471a3253eed1d93a7624dfc
sz9h <sam.s.zhou@newegg.com>
2020-06-27 17:43:02 +0800

    use iteration over recursion

--- itv/src/json-parser/tokenize.ts
+++ itv/src/json-parser/tokenize.ts
@@ -20,59 +20,68 @@ export type Token = {
 
 export let tokenize = (input: string): Token[] => {
   let result: Token[] = [];
+  let pointer = 0;
 
-  let _tokenize = (_input: string): void => {
-    let trimmed = _input.replace(
-      /^[\t\r\n\s]*([^\t\r\n\s]|([^\t\r\n\s].*[^\t\r\n\s]))[\t\r\n\s]*$/g,
-      (_, p) => p
-    );
-
-    if (trimmed === "") return;
-
-    let match = (pattern: string, type: TokenType, getContent = false) => {
-      if (RegExp(`^${pattern}`, "u").test(trimmed)) {
-        let content = RegExp(`^${pattern}`, "u").exec(trimmed)?.[0];
-        if (content === undefined) {
-          throw new Error(`Failed to match ${type}`);
-        }
-        if (getContent) {
-          if (type === "string") {
-            result.push({
-              type,
-              content: content.substring(1, content.length - 1) // remove ""
-            });
-          } else {
-            result.push({ type, content });
-          }
-        } else {
-          result.push({ type });
-        }
-        _tokenize(trimmed.substring(content.length || 1));
-        return true;
-      } else {
-        return false;
+  let match = (
+    pattern: string,
+    type: TokenType,
+    getContent = false
+  ) => (): Token | null => {
+    let regex = RegExp(`^[\\t\\r\\n\\s]*(${pattern})`, "u");
+    if (regex.test(input.substring(pointer))) {
+      let [whole, content] = regex.exec(
+        input.substring(pointer)
+      ) as RegExpExecArray;
+      if (content === undefined) {
+        throw new Error(`Failed to match ${type}`);
       }
-    };
-
-    function* matches() {
-      yield match("true", "true");
-      yield match("false", "false");
-      yield match("null", "null");
-      yield match("\\{", "curly-left");
-      yield match("\\}", "curly-right");
-      yield match("\\[", "bracket-left");
-      yield match("\\]", "bracket-right");
-      yield match(",", "comma");
-      yield match(":", "colon");
-      yield match(isNUMBER, "number", true);
-      yield match(isSTRING, "string", true);
-      throw new Error(`Invalid token at ${_input}`);
-    }
-    for (let m of matches()) {
-      if (m) return;
+      pointer += whole.length;
+      if (getContent) {
+        if (type === "string") {
+          return {
+            type,
+            content: content.substring(1, content.length - 1) // remove ""
+          };
+        } else {
+          return { type, content };
+        }
+      } else {
+        return { type };
+      }
+    } else {
+      return null;
     }
   };
 
-  _tokenize(input);
+  let matches = [
+    match("true", "true"),
+    match("false", "false"),
+    match("null", "null"),
+    match("\\{", "curly-left"),
+    match("\\}", "curly-right"),
+    match("\\[", "bracket-left"),
+    match("\\]", "bracket-right"),
+    match(",", "comma"),
+    match(":", "colon"),
+    match(isNUMBER, "number", true),
+    match(isSTRING, "string", true)
+  ];
+
+  while (pointer < input.length) {
+    let tmp: Token | null = null;
+    for (let m of matches) {
+      let res = m();
+      if (res !== null) {
+        tmp = res;
+        break;
+      }
+    }
+    if (tmp === null) {
+      throw new Error(`Invalid input at index: ${pointer}`);
+    } else {
+      result.push(tmp);
+    }
+  }
+
   return result;
 };
